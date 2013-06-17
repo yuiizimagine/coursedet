f05fe01c1d6746ec42eb7490e027e43325de2b30
Attakorn Poonkesorn <cartoonman_jr@hotmail.com>
2013-06-18 00:18:03 +0700

    edit table for support class user

--- register.sql
+++ register.sql
@@ -30,16 +30,18 @@ CREATE TABLE `register` (
   `gender` varchar(20) character set tis620 NOT NULL,
   `addr` varchar(255) character set tis620 NOT NULL,
   `birth_day` date NOT NULL,
-  `status` varchar(10) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `trainer` tinyint(1) NOT NULL,
+  `practitioner` tinyint(1) NOT NULL,
   PRIMARY KEY  (`member_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
 
 -- 
 -- dump ตาราง `register`
 -- 
 
-INSERT INTO `register` VALUES (23, 'L', '1', 'z', 1, 'aui', 'aa', 'male', 'A', '0000-00-00', 'user');
-INSERT INTO `register` VALUES (22, 'auimicky', '1234', 'auimicky@hotmail.com', 0, 'Kanokporn', 'Ruanruen', 'female', 'Korea ', '0000-00-00', 'user');
-INSERT INTO `register` VALUES (24, 'cartoonmed', 'cartoonman', 'cartoonman_jr@hotmail.com', 0, 'attakorn', 'poonkesorn', 'male', '111/2', '1991-04-01', 'user');
-INSERT INTO `register` VALUES (25, 'jkme', 'jkmee', 'yuiizimagine@gmail.com', 1, 'juthathip', 'kaensing', '', '99/99', '1991-04-09', 'admin');
-INSERT INTO `register` VALUES (30, 'fff', 'fff', 'fff', 0, 'fff', 'fff', 'female', 'fff', '2001-07-03', 'user');
+INSERT INTO `register` VALUES (23, 'L', '1', 'z', 1, 'aui', 'aa', 'male', 'A', '0000-00-00', 'user', 0, 0);
+INSERT INTO `register` VALUES (22, 'auimicky', '1234', 'auimicky@hotmail.com', 0, 'Kanokporn', 'Ruanruen', 'female', 'Korea ', '0000-00-00', 'user', 0, 0);
+INSERT INTO `register` VALUES (24, 'cartoonmed', 'cartoonman', 'cartoonman_jr@hotmail.com', 0, 'attakorn', 'poonkesorn', 'male', '111/2', '1991-04-01', 'admin', 0, 0);
+INSERT INTO `register` VALUES (25, 'jkme', 'jkmee', 'yuiizimagine@gmail.com', 1, 'juthathip', 'kaensing', '', '99/99', '1991-04-09', 'admin', 0, 0);
+INSERT INTO `register` VALUES (31, 'ccc', 'ccc', 'aaa', 1, 'aaa', 'aaa', 'female', 'aaa', '2013-01-01', 'user', 1, 1);
